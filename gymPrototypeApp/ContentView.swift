dd01660d8d19a3370e8eb5edeee7543117aa6640
matheus3006 <matheussouza18@live.com>
2022-06-04 14:49:43 -0300

    Initial Commit

---
+++ gymPrototypeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gymPrototypeApp
+//
+//  Created by Matheus Souza on 6/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gymPrototypeApp/gymPrototypeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymPrototypeAppApp.swift
+//  gymPrototypeApp
+//
+//  Created by Matheus Souza on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct gymPrototypeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
